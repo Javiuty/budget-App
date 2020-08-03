41b8d855de9a2efd5991aa8055975736b4fec05c
Javiuty <javiiescuadra@gmail.com>
2020-08-03 13:56:50 +0200

    control flow expenses and incomes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var budgetController = (function() {
         this.description = description;
         this.value = value;
     };
-
+    
     var data = {
         allItems: {
             exp: [],
@@ -22,7 +22,20 @@ var budgetController = (function() {
             exp: 0,
             inc: 0,
         }    
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val);
+            } else if (type === "inc") {
+                newItem = new Income(ID, des, val);
+            }
+
+        }
+    };
 
 })();
 
