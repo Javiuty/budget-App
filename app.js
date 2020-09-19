615e47d3120b055d6a2650507175461376f90bb0
Javiuty <javiiescuadra@gmail.com>
2020-09-19 21:10:17 +0200

    setting up deleting eventListener + some styles

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
+    container: ".container",
   };
 
   return {
@@ -183,6 +184,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
   };
 
   var updateBudget = function () {
@@ -216,6 +219,28 @@ var controller = (function (budgetCtrl, UICtrl) {
       updateBudget();
     }
   };
+
+  var ctrlDeleteItem = function(event) {
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+
+      //inc-1
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. Delete the item from the data structure
+
+      // 2. Delete the item from the UI
+
+      // 3. Update and show the new budget
+
+    }
+  }
+
 
   return {
     init: function () {
