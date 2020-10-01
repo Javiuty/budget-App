3a358f193cc01bb0c9cfcfde510e9ce383f20365
Javiuty <javiiescuadra@gmail.com>
2020-10-01 12:05:39 +0200

    deleteItem method

--- app.js
+++ app.js
@@ -57,6 +57,20 @@ var budgetController = (function () {
       return newItem;
     },
 
+    deleteItem: function (type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function () {
       // calculate total income and expenses
       calculateTotal("exp");
@@ -220,27 +234,24 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
   };
 
-  var ctrlDeleteItem = function(event) {
+  var ctrlDeleteItem = function (event) {
     var itemID, splitID, type, ID;
 
     itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 
     if (itemID) {
-
       //inc-1
-      splitID = itemID.split('-');
+      splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // 1. Delete the item from the data structure
-
+      budgetCtrl.deleteItem(type, ID);
       // 2. Delete the item from the UI
 
       // 3. Update and show the new budget
-
     }
-  }
-
+  };
 
   return {
     init: function () {
